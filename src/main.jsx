e95a8da096b9e6fdc64214ab357a75ca0713bcbd
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-03-25 20:10:08 +0600

    Navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,24 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import ListedBooks from './Components/ListedBooks/ListedBooks';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/listedBooks",
+        element: <ListedBooks></ListedBooks>
+      }
+    ]
   },
 ]);
 
