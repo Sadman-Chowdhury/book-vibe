99ea5200bfa929ad9b9510722a03916cffcb6b2d
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-03-26 18:02:05 +0600

    Wishlist books tab added in Listed books page

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -24,12 +24,12 @@ const ListedBooks = () => {
     },[books])
 
     return (
-        <div>
+        <div className="mt-10">
             <div role="tablist" className="tabs tabs-lifted">
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read Books" checked />
+                <input type="radio" name="my_tabs_2" role="tab" className="tab text-xl work-sans" aria-label="Read Books" checked />
                 <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
                     {
-                        readBooks.map(readBook=><div key={readBook.bookId} className="flex flex-col lg:flex-row work-sans gap-10 mt-6">
+                        readBooks.map(readBook=><div key={readBook.bookId} className="flex flex-row work-sans gap-10 mt-10">
                         <div className="bg-gray-100 rounded-2xl flex justify-center p-8 w-3/12">
                             <img className="h-[170px]" src={readBook.image} alt="" />
                         </div>
@@ -71,8 +71,50 @@ const ListedBooks = () => {
                     }
                 </div>
 
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist Books" />
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 3</div>
+                <input type="radio" name="my_tabs_2" role="tab" className="tab text-xl work-sans" aria-label="Wishlist Books" />
+                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+                {
+                        wishlistBooks.map(wishlistBook=><div key={wishlistBook.bookId} className="flex flex-row work-sans gap-10 mt-10">
+                        <div className="bg-gray-100 rounded-2xl flex justify-center p-8 w-3/12">
+                            <img className="h-[170px]" src={wishlistBook.image} alt="" />
+                        </div>
+                        <div className="w-3/4 space-y-3">
+                            <h1 className="text-[24px] font-bold playfair">{wishlistBook.bookName}</h1>
+                            <p className="text-[16px] font-medium">By: {wishlistBook.author}</p>
+                            <div className="flex gap-5">
+                                <div className="space-x-5">
+                                    <span className="font-bold text-black">Tag</span>
+                                    <span className="text-[#23BE0A] bg-gray-100 px-4 py-1 rounded-[30px]">#{wishlistBook.tags[0]}</span>
+                                    <span className="text-[#23BE0A] bg-gray-100 px-4 py-1 rounded-[30px]">#{wishlistBook.tags[1]}</span>
+                                </div>
+                                <div className="flex text-[#131313B3]">
+                                    <MdOutlineLocationOn className="text-2xl"/>
+                                    <p>Year of Publishing:  </p>
+                                    <p className="pl-2">{wishlistBook.yearOfPublishing}</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-10 text-[#131313B3]">
+                                <div className="flex gap-3">
+                                    <IoMdContacts className="text-2xl"/>
+                                    <p>Publisher: </p>
+                                    <p>{wishlistBook.publisher}</p>
+                                </div>
+                                <div className="flex gap-1">
+                                    <HiOutlineDocumentChartBar className="text-2xl"/>
+                                    <p>Page: </p>
+                                    <p>{wishlistBook.totalPages}</p>
+                                </div>
+                            </div>
+                            <hr className="border-dashed"/>
+                            <div className="flex gap-6">
+                                <span className="px-5 py-2 bg-blue-100 rounded-[30px] text-[#328EFF]">Category: {wishlistBook.category}</span>
+                                <span className="px-5 py-2 bg-orange-100 rounded-[30px] text-[#FFAC33]">Rating: {wishlistBook.rating}</span>
+                                <Link to={`/bookDetails/${wishlistBook.bookId}`}><button className="px-5 py-2 bg-green-500 rounded-[30px] text-white">View Details</button></Link>
+                            </div>
+                        </div>
+                    </div>)
+                    }
+                </div>
             </div>
         </div>
     );
